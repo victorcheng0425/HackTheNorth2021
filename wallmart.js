69c3b363bc707b51cf32c4fc85b46cdf3f5969d4
Ken Obata <kenichiderumo@gmail.com>
2021-09-18 16:47:14 -0700

    commit2: wallmart dealing with multiple products.

--- wallmart.js
+++ wallmart.js
@@ -4,38 +4,39 @@ const puppeteer = require('puppeteer')
         keyword:str
 return:
         price, description, image*/
-async function wallmart_scraper(keyword){
+
+let wallmart_scraper = async(keyword) => {
     const url='https://www.walmart.ca/search?q='+keyword;
     const browser = await puppeteer.launch({
         executablePath: '/usr/local/bin/chromium',
         headless: false
     })
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log("Start:" + keyword);
 
-    try{
-        const page = await browser.newPage();
-        await page.goto(url);
+    let productNames = await page.evaluate(()=>{
+        console.log("hello");
+        let div = document.querySelectorAll("div[data-automation='product']");
 
-        //const [el] = await page.$x('//*[@id="product-results"]/div[1]/div/a/div/div[1]/img');
-        const [el] = await page.$x('/html/body/div[1]/div[1]/div[3]/div/div/div/div/div[7]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div/a/div/div[1]/img');
-        const src = await el.getProperty('src');
-        const imageURL = await src.jsonValue();
+        let products = []
 
+        div.forEach(element => {
+            let price = element.querySelector("div[data-automation='current-price']");
+            if(price != null){
+                products.push(price.textContent);
+            }
+        });
+        console.log(products)
+        return products
+    })
+    console.log("end wallmart scraping.");
+    await browser.close();
+    return productNames;
 
-        const [el2] = await page.$x('//*[@id="product-results"]/div[1]/div/a/div/div[2]/div[2]/span/div/p');
-        const txt = await el2.getProperty('textContent');
-        const title = await txt.jsonValue();
+};
 
-        const [el3] = await page.$x('//*[@id="product-results"]/div[1]/div/a/div/div[2]/div[3]/div[1]/div/span/span');
-        const price = await el3.getProperty('textContent');
-        const priceTxt = await price.jsonValue();
-        console.log({imageURL, title, priceTxt});
-    } catch (err){
-        console.error(err.message);
-    } finally {
-        await browser.close();
+wallmart_scraper('shoes').then((value => {
+        console.log("returned.");
     }
-}
-
-
-
-wallmart_scraper('glove')
+));
