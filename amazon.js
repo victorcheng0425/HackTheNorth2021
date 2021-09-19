e44955f5d6945a8fb4369ca1966df621ab51706a
dongandy <andydong0928@gmail.com>
2021-09-19 01:37:05 -0700

    title visible, but not all correct

--- amazon.js
+++ amazon.js
@@ -36,18 +36,24 @@ async function amazon_scraper(keyword) {
             let image = element.getElementsByTagName("img")[0].getAttribute("src");
             let price = document.getElementsByClassName("a-offscreen")[i++];
             // let title = document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal")[0];
+            let title = element.getElementsByTagName("span")[2];
             //TODO: Fix the case where there is missing price (need to shift)
             //TODO: Add title
 
-            // if(price!=null){
-            //     price = price.textContent.replace('.','');
-            //     console.log("price :", price.textContent)
-            // } else{
-            //     price = "";
-            // }
+            if(price!=null){
+                // price = price.textContent.replace('.','');
+                console.log("price :", price.textContent)
+            } else{
+                price = "";
+            }
+            if(title!=null){
+                title = title.textContent;
+            } else {
+                title = "";
+            }
             temp.push(
                 {
-                    // 'title': title.innerText,
+                    'title': title,
                     'price': price.innerText,
                     'image': image,
                     'link': 'https://www.amazon.ca/' + href
