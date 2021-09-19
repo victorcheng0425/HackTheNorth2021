f5e4d2b741474ca58c0f357bfa4f5eee9fd30876
Ken Obata <kenichiderumo@gmail.com>
2021-09-18 17:52:53 -0700

    commit3: function returns multiple products with float.

--- wallmart.js
+++ wallmart.js
@@ -3,40 +3,58 @@ const puppeteer = require('puppeteer')
 /*input:
         keyword:str
 return:
-        price, description, image*/
+        price, price, image, url*/
 
 let wallmart_scraper = async(keyword) => {
+    /* 1. Setup and configurations*/
     const url='https://www.walmart.ca/search?q='+keyword;
     const browser = await puppeteer.launch({
         executablePath: '/usr/local/bin/chromium',
         headless: false
     })
     const page = await browser.newPage();
+    //uncomment if you want to debug!
+    //page.on('console', (log) => console[log._type](log._text));
+    /*page.on('console', consoleMessageObject => function (consoleMessageObject) {
+        if (consoleMessageObject._type !== 'warning') {
+            console.log(consoleMessageObject._text)
+        }
+    });*/
+
     await page.goto(url);
     console.log("Start:" + keyword);
 
-    let productNames = await page.evaluate(()=>{
+    let productNames = await page.evaluate(() => {
         console.log("hello");
-        let div = document.querySelectorAll("div[data-automation='product']");
-
         let products = []
-
-        div.forEach(element => {
-            let price = element.querySelector("div[data-automation='current-price']");
-            if(price != null){
-                products.push(price.textContent);
-            }
+        let product_items = document.querySelectorAll("div[data-automation='product']");
+        console.log("product_items.length", product_items.length)
+        product_items.forEach(element => {
+            let title = element.getElementsByTagName("p")[0];
+            let price = element.querySelector("div[data-automation='price-section-wrapper']");
+            let price_string = price.textContent.replace('$','');
+            let price_float = parseFloat(price_string);
+            let image = element.getElementsByTagName("img")[0];
+            let url = "https://www.walmart.ca/" +
+                element.getElementsByTagName("a")[0].getAttribute("href");
+            //console.log(title.textContent, price.textContent, image.getAttribute("src"),url)
+            products.push(
+                {
+                    'title': title.textContent,
+                    'price': price_float,
+                    'image': image.getAttribute("src"),
+                    'link': url,
+                }
+            );
         });
-        console.log(products)
         return products
-    })
+    });
     console.log("end wallmart scraping.");
     await browser.close();
     return productNames;
-
 };
 
-wallmart_scraper('shoes').then((value => {
-        console.log("returned.");
+wallmart_scraper('T-Shirt').then((value => {
+        console.log(value);
     }
 ));
