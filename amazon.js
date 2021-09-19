59d0c2b6bdaf2b08a6357f4ac5db7f13a97e02b1
dongandy <andydong0928@gmail.com>
2021-09-19 02:10:54 -0700

    new commit

--- amazon.js
+++ amazon.js
@@ -1,25 +1,9 @@
-// console.log("first");
-// const [el] = await page.$x('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/div[2]/div/span/a/div/img');
-// const src = await el.getProperty('src');
-// const image = await src.jsonValue();
-//
-// console.log("second");
-// const [el2] = await page.$x('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/div[3]/div[1]/h2/a');
-// const txt = await el2.getProperty('textContent');
-// const title = await txt.jsonValue();
-//
-// console.log("third");
-// const [el3] = await page.$x('//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/div[3]/div[3]/div/a/span/span[1]');
-// const price = await el3.getProperty('textContent');
-// const priceTxt = await price.jsonValue();
-// console.log({image, title, priceTxt});
-// temp.push({image, title, priceTxt});
-
 const puppeteer = require('puppeteer');
+const {praseToPrice} = require("./ebay");
 // const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 // puppeteer.use(StealthPlugin());
 
-async function amazon_scraper(keyword) {
+let amazon_scraper = async(keyword) => {
     const url = 'https://www.amazon.ca/s?k=' + keyword;
     const browser = await puppeteer.launch({
         // headless: false
@@ -36,9 +20,13 @@ async function amazon_scraper(keyword) {
             let image = element.getElementsByTagName("img")[0].getAttribute("src");
             let price = document.getElementsByClassName("a-offscreen")[i++];
             // let title = document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal")[0];
-            let title = element.getElementsByTagName("span")[2];
+            let title;
+            if (element.getElementsByClassName('a-size-base-plus a-color-base a-text-normal')) {
+                title = element.getElementsByTagName("span")[2];
+            }
+
             //TODO: Fix the case where there is missing price (need to shift)
-            //TODO: Add title
+            //TODO: Add title (fix bugs like 'sponsor')
 
             if(price!=null){
                 // price = price.textContent.replace('.','');
@@ -66,6 +54,6 @@ async function amazon_scraper(keyword) {
     return result;
 }
 
-amazon_scraper('apple watch').then((result => {
-    console.log(result);
-}))
+module.exports = {
+    amazon_scraper
+}
